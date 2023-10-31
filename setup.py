4b3c056e49f672b0807abb4612548cb2a00a0e4a
ZealExorcist <141896261+ZealExorcist@users.noreply.github.com>
2023-10-31 09:51:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vsl',
+    version='0.1.0',
+    author='BumpyRohan',
+    author_email='rohan.nayakanti@gmail.com',
+    packages=['vsl'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
